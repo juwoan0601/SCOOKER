b603465c3158aa9f6e24b5bbc0c351c20b3f2a4d
Juwan HAN <56291373+juwoan0601@users.noreply.github.com>
2021-02-07 22:41:20 +0900

    Update BLE_Gyro_App_PCB_20201207.ino

--- BLE_Gyro_App_PCB_20201207.ino
+++ BLE_Gyro_App_PCB_20201207.ino
@@ -3,8 +3,12 @@
 // This version's MPU initialized the scale "+-2000dps" and "+-16g"
 // It can affect your device condition
 // The Helmet Blackbox Project <<SCOOKER>>
-
+// When you turn on the Device, it will be automatically Calibration about least 2 min.
+// So, Release the device on the flat place.
 // Written by GMObean
+
+//v2.4.0 Add Calculate linear velocity function
+
 //*========================================
 
 #include <SoftwareSerial.h> 
@@ -113,6 +117,14 @@ int bluePin = 4;
  */
 int Accel_scale = 16;
 
+// Calculate Linear velocity
+float b_Axyz[3] = {0.0};
+float velocity[3] = {0.0};
+float currAccel[3] = {0.0};
+float prevAccel[3] = {0.0};
+unsigned long prevTime = millis();
+unsigned long currTime;
+
 // Bluetooth Setting
 SoftwareSerial BTSerial(BT_TX, BT_RX); // Software Serial (TX,RX) 
 
@@ -125,13 +137,21 @@ void setup(){
   Wire.begin();
   Serial.begin(38400);  // 통신속도 38400 bps
   BTSerial.begin(9600);
-  //Serial.println("SCOOKER Firmware v2.1.1");
-  //Serial.println("Initializing I2C devices...");
+  char FWversion[10] = "v2.4.0";
+  BTSerial.write(FWversion);
+  BTSerial.write('\n');
+  //Firmware Info
+  Serial.print("SCOOKER Firmware| ");
+  Serial.println(FWversion);
+  Serial.println("Initializing I2C devices...");
   accelgyro.initialize();
   // verify connection
-  //Serial.println("Testing device connections...");
-  //Serial.println(accelgyro.testConnection() ? "MPU9250 connection successful" : "MPU9250 connection failed");
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU9250 connection successful" : "MPU9250 connection failed");
+  Axyz_init_calibrated(); // Accelerometer data has been calibrated here
+  //Mxyz_init_calibrated(); // Magnetometer data has been calibrated here
   
+  /*
   // verify LED connection
   setColor(255,255,255);
   delay(2000);
@@ -142,98 +162,173 @@ void setup(){
   setColor(0,0,255);
   delay(200);
   setColor(0,0,0);
+  */
 }
 
 void loop(){
-  char data[100] = {0} ; // Data formet
   // Gyro Sensing Process
     getAccel_Data();
     getGyro_Data();
     getCompassDate_calibrated(); // compass data has been calibrated here
     getHeading();               //before we use this function we should run 'getCompassDate_calibrated()' frist, so that we can get calibrated data ,then we can get correct angle .
     getTiltHeading();
+    //Serial.print("Heading Value: ");
+    //Serial.print(heading);
+    //Serial.print(',');
+    //Serial.println(tiltheading);
 
   // Generating Data
-    //Serial.println("Acceleration(g) of X,Y,Z:");
-    //Serial.print(Axyz[0]);
-    //Serial.print(",");
-    //Serial.print(Axyz[1]);
-    //Serial.print(",");
-    //Serial.println(Axyz[2]);
-    //Serial.print(Axyz[2]);
-    //Serial.print(",");
-    //Serial.println("Angular Velocity (Degree per sec) of X,Y,Z:");
-    //Serial.print(Gxyz[0]);
-    //Serial.print(",");
 
-
-    
+    /*
+    Serial.println("Acceleration(g) of X,Y,Z:");
+    Serial.print(Axyz[0]);
+    Serial.print(",");
+    Serial.print(Axyz[1]);
+    Serial.print(",");
+    Serial.println(Axyz[2]);
+    */
+    /*
+    Serial.println("Angular Velocity (Degree per sec) of X,Y,Z:");
+    Serial.print(Gxyz[0]);
+    Serial.print(",");
     Serial.print(Gxyz[1]);
     Serial.print(",");
-    int i;
-    int svalue = 0;
-    int numReadings = 10;
-
-    for (i = 0; i < numReadings; i++){
-    getGyro_Data();
-    svalue = svalue + Gxyz[1];
-    // 1ms pause adds more stability between reads.
-    delay(1);
-    }
-    svalue = svalue / numReadings;
-
-    Serial.println(svalue);
-  
-    //Serial.print(",");
-    //Serial.println(Gxyz[2]);
-    //Serial.println(Gxyz[2]);
-    //Serial.print(",");
+    Serial.println(Gxyz[2]);
+    */
+    /*
     //Serial.println("Campass (Degree) of X,Y,Z:");
-    //Serial.print(Mxyz[0]);
-    //Serial.print(",");
-    //Serial.print(Mxyz[1]);
-    //Serial.print(",");
-    //Serial.println(Mxyz[2]);
-    float avg_acc;
-    float avg_gyro;
-    avg_acc = sqrt((Axyz[0])*(Axyz[0])+(Axyz[1])*(Axyz[1])+(Axyz[2])*(Axyz[2]));
-    avg_gyro = sqrt((Gxyz[0])*(Gxyz[0])+(Gxyz[1])*(Gxyz[1])+(Gxyz[2])*(Gxyz[2]));
-    //Serial.print("vector size of acceleration: ");
-    //Serial.println(avg_acc);
-    //Serial.print("vector size of Angular Velocity: ");
-    //Serial.println(avg_gyro);
-    long scaleUp_acc;
-    scaleUp_acc = avg_acc*100;
-    long Ax=0;
-    long Ay=0;
-    long Az=0;
-    char valuelist[10];
-    long value;
-    
-    Ax = abs(Axyz[0]*100);
-    Ay = abs(Axyz[1]*100);
-    Az = abs(Axyz[2]*100);
-    //Serial.print(abs(Ax));
-    //Serial.print(abs(Ay));
-    //Serial.println(abs(Az));
-    value = Ax*1000000;
-    delay(5);
-    value = value+Ay*1000;
-    delay(5);
-    value = value+Az;
-    
-    ltoa(value,valuelist, 10);    
-    
-    //Serial.print("Sending Data: ");
-    //Serial.println(valuelist);
-    //ltoa(valuelist, data, 10);
-    LEDLevel(scaleUp_acc);
-    // Sending Data
-    //data = valuelist;
-    BTSerial.write(valuelist);
-    BTSerial.write("\n");
+    Serial.print(Mxyz[0]);
+    Serial.print(",");
+    Serial.print(Mxyz[1]);
+    Serial.print(",");
+    Serial.println(Mxyz[2]);
+    */
 
-    //delay(70);
+    // Sending Format
+    
+    int int_Axyz[3] = {0};
+    int int_Gxyz[3] = {0};
+    int int_Mxyz[3] = {0};
+    char accellist[19] = {0};   //******a******a******a
+    char gyrolist[25] = {0};    //********a********a********a 
+    char compasslist[19] = {0}; //******a******a******a
+
+    int i = 0;
+    // Float data to char
+    for(i=0; i < 3; i++){
+    int_Axyz[i] = Axyz[i]*100;
+    int_Gxyz[i] = Gxyz[i]*100;
+    int_Mxyz[i] = Mxyz[i]*100;
+    }
+
+    char partition_char = 'a';
+
+    for(i=0; i < 3; i++){
+      accellist[i*6+5] = partition_char;
+      gyrolist[i*8+7] = partition_char;
+      compasslist[i*6+5] = partition_char;
+    }
+
+    // Define sign of number
+    for(i=0; i < 3; i++){
+      if(int_Axyz[i] > 0){accellist[i*6]='0';}
+      else {accellist[i*6]= '1';}
+      if(int_Gxyz[i] > 0){gyrolist[i*8]='0';}
+      else {gyrolist[i*8]= '1';}
+      if(int_Mxyz[i] > 0){compasslist[i*6]='0';}
+      else {compasslist[i*6]= '1';}
+    }
+    
+    for(i=0; i < 3; i++){
+      int_Axyz[i] = abs(int_Axyz[i]);
+      int_Gxyz[i] = abs(int_Gxyz[i]);
+      int_Mxyz[i] = abs(int_Mxyz[i]);
+    }
+
+    for(i=0; i < 3; i++){
+      accellist[i*6+1] = int_Axyz[i]/1000 + '0';
+      int_Axyz[i] = int_Axyz[i] - (int_Axyz[i]/1000)*1000;
+      accellist[i*6+2] = int_Axyz[i]/100 + '0';
+      int_Axyz[i] = int_Axyz[i] - (int_Axyz[i]/100)*100;
+      accellist[i*6+3] = int_Axyz[i]/10 + '0';
+      int_Axyz[i] = int_Axyz[i] - (int_Axyz[i]/10)*10;
+      accellist[i*6+4] = int_Axyz[i] + '0';
+
+      gyrolist[i*8+1] = int_Gxyz[i]/100000 + '0';
+      int_Gxyz[i] = int_Gxyz[i] - (int_Gxyz[i]/100000)*100000;
+      gyrolist[i*8+2] = int_Gxyz[i]/10000 + '0';
+      int_Gxyz[i] = int_Gxyz[i] - (int_Gxyz[i]/10000)*10000;
+      gyrolist[i*8+3] = int_Gxyz[i]/1000 + '0';
+      int_Gxyz[i] = int_Gxyz[i] - (int_Gxyz[i]/1000)*1000;
+      gyrolist[i*8+4] = int_Gxyz[i]/100 + '0';
+      int_Gxyz[i] = int_Gxyz[i] - (int_Gxyz[i]/100)*100;
+      gyrolist[i*8+5] = int_Gxyz[i]/10 + '0';
+      int_Gxyz[i] = int_Gxyz[i] - (int_Gxyz[i]/10)*10;
+      gyrolist[i*8+6] = int_Gxyz[i] + '0';
+
+      compasslist[i*6+1] = int_Mxyz[i]/1000 + '0';
+      int_Mxyz[i] = int_Mxyz[i] - (int_Mxyz[i]/1000)*1000;
+      compasslist[i*6+2] = int_Mxyz[i]/100 + '0';
+      int_Mxyz[i] = int_Mxyz[i] - (int_Mxyz[i]/100)*100;
+      compasslist[i*6+3] = int_Mxyz[i]/10 + '0';
+      int_Mxyz[i] = int_Mxyz[i] - (int_Mxyz[i]/10)*10;
+      compasslist[i*6+4] = int_Mxyz[i] + '0';
+    }
+    
+    //Serial.println("Sending Data: ");
+    /*
+    Serial.print(accellist);
+    Serial.print(gyrolist);
+    Serial.println(compasslist);
+    */
+    char valuelist[62] = {0};
+
+    for(i=0; i < 18; i++){
+      valuelist[i]=accellist[i];
+    }
+    for(i=0; i < 24; i++){
+      valuelist[i+18]=gyrolist[i];
+    }
+    for(i=0; i < 18; i++){
+      valuelist[i+42]=compasslist[i];
+    }
+    
+    //Serial.println(valuelist);
+
+    BTSerial.write(valuelist);
+    BTSerial.write('\n');
+    delay(1);
+
+    // Calculate linear velocity
+    currTime = millis();
+    for(i = 0; i < 3; i++){
+      currAccel[i]= (Axyz[i]-b_Axyz[i])*9.81;
+      if(currAccel[i] < 0.1) currAccel[i]=0;
+      velocity[i] += (currAccel[i] + prevAccel[i])/2*(currTime - prevTime)/1000; //1000 added to get the same units
+      prevAccel[i] = currAccel[i];
+    }
+    prevTime = currTime;
+    /*
+    Serial.print(Axyz[0]);
+    Serial.print(',');
+    Serial.print(Axyz[1]);
+    Serial.print(',');
+    Serial.print(Axyz[2]);
+    Serial.println(',');
+    */
+    
+    for(i=0; i<3; i++){
+      Serial.print(velocity[i]);
+      Serial.print(',');
+    }
+    
+    for(i=0; i<3; i++){
+      Serial.print(Axyz[i]-b_Axyz[i]);
+      Serial.print(',');
+    }
+    Serial.print('\n');
+    
+    delay(1);
 }
 
 
@@ -276,6 +371,37 @@ void getTiltHeading(void)
     tiltheading = 180 * atan2(yh, xh) / PI;
     if (yh < 0)    tiltheading += 360;
 }
+void Axyz_init_calibrated ()
+{
+  Serial.println("Calibration Accelometer. Please put the device on an FLAT SURFACE for 10 sec");
+  delay(500);
+  int i = 0;
+  int j = 0;
+  int n = 20; // data sampling number 
+  float temp[3] = {0};
+  for(i=0; i < n; i++){
+    getAccel_Data();
+    for(j=0; j<3; j++){
+      Serial.print(Axyz[j]);
+      Serial.print(',');
+    }
+    Serial.print('\n');
+    for(j=0; j < 3; j++){
+      temp[j]+= Axyz[j]; 
+    }
+  }
+  for(i=0; i < 3; i++){
+      b_Axyz[i] = temp[i]/n;
+  }
+
+  Serial.print ("Accelerometer Calibration value: ");
+  for(i=0; i<3; i++){
+      Serial.print(b_Axyz[i]);
+      Serial.print(',');
+  }
+  Serial.print('\n');
+}
+
 void Mxyz_init_calibrated ()
 {
 
@@ -284,7 +410,7 @@ void Mxyz_init_calibrated ()
     Serial.println(F("During  calibratting ,you should rotate and turn the 9DOF all the time within 2 minutes."));
     Serial.print("  ");
     Serial.println(F("If you are ready ,please sent a command data 'ready' to start sample and calibrate."));
-    while (!Serial.find("ready"));
+    //while (!Serial.find("ready"));
     Serial.println("  ");
     Serial.println("ready");
     Serial.println("Sample starting......");
